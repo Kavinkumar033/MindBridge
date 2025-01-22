ffbc4108ef1cd16267ffacd29303bfd2d61909ca
Kavinkumar033 <kavinkumar1204.s@gmail.com>
2025-01-22 09:34:23 +0530

    Uploaded

--- CoreJava/Day15/TopScorer.java
+++ CoreJava/Day15/TopScorer.java
@@ -18,47 +18,13 @@ public class TopScorer {
         list.add(s4);
         list.add(s5);
 
-
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*Map<String, Optional<Students>> topScorersByDept = list.stream()
+    Map<String, Optional<Students>> topScorersByDept = list.stream()
         .collect(Collectors.groupingBy(Students::getDept,
                 Collectors.maxBy(Comparator.comparingDouble(Students::getCgpa))
         ));
         topScorersByDept.forEach((department, student) -> {
         student.ifPresent(s -> System.out.println(department + " - Top Scorer: " + s));
-        });*/
+        });
+    }
+}
+
